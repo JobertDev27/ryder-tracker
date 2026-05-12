f85c9580a90c847f0f5d3b531793619954e7a746
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-05-12 20:22:56 +0800

    fix: added more icon type and screenshot for rich install

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,6 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#ffffff",
     icons: [
       {
+        src: "/web-app-manifest-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
         src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
@@ -23,6 +29,21 @@ export default function manifest(): MetadataRoute.Manifest {
         type: "image/png",
         purpose: "maskable",
       },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
     ],
+    screenshots: [
+      {
+        src: "/screenshot.png",
+        sizes: "1080x1920",
+        type: "image/png",
+      },
+    ],
+
+    categories: ["finance", "productivity"],
   };
 }
