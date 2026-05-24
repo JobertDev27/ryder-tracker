9aab2b15be76dcac772975d76aab8d29ae98c995
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-05-24 19:18:25 +0800

    refac: removed unnecesary index

--- lib/db.ts
+++ lib/db.ts
@@ -6,12 +6,21 @@ interface Delivered {
   amount: number;
 }
 
+interface WeeklyGoal {
+  id: number;
+  overtime: number;
+  delivered: number;
+  remaining: number;
+}
+
 export const db = new Dexie("ryderDB") as Dexie & {
   delivered: EntityTable<Delivered, "id">;
+  weeklyGoal: EntityTable<WeeklyGoal, "id">;
 };
 
 db.version(1).stores({
-  delivered: "++id, date, amount",
+  delivered: "++id, date",
+  weeklyGoal: "++id",
 });
 
 export type { Delivered };
