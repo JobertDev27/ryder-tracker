86fb5734bf9b49d387a2c83e92ecd34f764a1e43
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-05-24 20:30:20 +0800

    feat: added increment, decrementand query all

--- utils/database.ts
+++ utils/database.ts
@@ -1,3 +1,39 @@
-import { db } from "@/lib/db";
+import { db, Delivered } from "@/lib/db";
 
-export function addDelivery() {}
+const today = new Date().toISOString().split("T")[0];
+
+export async function getDeliveries() {
+  const deliveries = await db.delivered.toArray();
+  return deliveries;
+}
+
+export async function incrementDelivery() {
+  try {
+    const exist = await db.delivered.where("date").equals(today).first();
+
+    if (exist) {
+      await db.delivered.update(exist.id!, {
+        amount: exist.amount + 1,
+      });
+    } else {
+      await db.delivered.add({
+        date: today,
+        amount: 1,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function decrementDelivery() {
+  const deliveredToday = await db.delivered.where("date").equals(today).first();
+  if (!deliveredToday) return;
+  try {
+    await db.delivered.update(deliveredToday.id!, {
+      amount: deliveredToday.amount - 1,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
