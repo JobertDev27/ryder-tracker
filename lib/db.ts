7d0f5f6ed30799299ae3e1927226b213cdb4f723
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-05-25 15:58:37 +0800

    refac: removed weekly table

--- lib/db.ts
+++ lib/db.ts
@@ -1,26 +1,17 @@
 import Dexie, { EntityTable } from "dexie";
 
-interface Delivered {
+interface DeliveredProp {
   id: number;
   date: string;
   amount: number;
 }
 
-interface WeeklyGoal {
-  id: number;
-  overtime: number;
-  delivered: number;
-  remaining: number;
-}
-
 export const db = new Dexie("ryderDB") as Dexie & {
-  delivered: EntityTable<Delivered, "id">;
-  weeklyGoal: EntityTable<WeeklyGoal, "id">;
+  delivered: EntityTable<DeliveredProp, "id">;
 };
 
 db.version(1).stores({
   delivered: "++id, date",
-  weeklyGoal: "++id",
 });
 
-export type { Delivered };
+export type { DeliveredProp };
